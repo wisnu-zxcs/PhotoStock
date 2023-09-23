202315e6320f71683f5f4afb28cccabc1ae1c5a5
Wisnu Putra Jiwangga <128898605+wisnu-zxcs@users.noreply.github.com>
2023-09-24 04:45:33 +0900

    Delete PhotoStock directory

--- PhotoStock/assets/scripts/javascript/loginPage-front.js
+++
@@ -1,7 +0,0 @@
-const totalCount = 23;
-
-function backgroundChanger() {
-  const num = Math.ceil(Math.random() * totalCount);
-
-  document.body.style.background = `url('assets/images/backgrounds/${num}.jpeg') center/cover no-repeat fixed`;
-}
