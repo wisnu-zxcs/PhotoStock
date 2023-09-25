2f9e75565a7954b9a5dba8b72451d1417986d377
Wisnu Putra Jiwangga <128898605+wisnu-zxcs@users.noreply.github.com>
2023-09-26 01:01:31 +0900

    Delete assets directory

--- assets/scripts/javascript/loginPage-front.js
+++
@@ -1,8 +0,0 @@
-// Random background picture processor
-const totalPicture = 54;
-
-function backgroundChanger() {
-  const randomGenerator = Math.ceil(Math.random() * totalPicture);
-
-  document.querySelector("body").style.background = `url('assets/images/backgrounds/${randomGenerator}.jpg') center/cover no-repeat fixed`;
-}
